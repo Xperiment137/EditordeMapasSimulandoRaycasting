4c066a0b2760b76feeba81c3abad32f34df8f91a
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2019-08-13 20:23:57 +0200

    Add files via upload

---
+++ EditorMapas2.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+public class EditorMapas2 : MonoBehaviour
+{
+    private Quaternion newQuaternion;
+    public Terrain aux;
+    public Mesh x2;
+    private Vector3[] vertices;
+    public GameObject pared;
+    private int con = 0;
+    public float x = 0;
+    public float z = 0;
+    public int tope = 0;
+    public int tope1 = 0;
+    public int tope2 = 0;
+    public int tope3 = 0;
+    public int i = 0;
+    void Awake()
+    {
+         newQuaternion = new Quaternion();
+        x2 = new Mesh();
+        aux = GetComponent<Terrain>();
+        GetComponent<MeshFilter>().mesh = x2; // lo añades pero autogenera la "figura".
+        vertices = x2.vertices;
+        x2.RecalculateBounds();//calcular el volumen delimitador de la malla a partir de los vértices.
+        pared = (GameObject)PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath("Assets/3Dmap/Pared.prefab", typeof(GameObject)));
+        StartCoroutine(CalulateShape());
+    }
+    void Start()
+    {
+
+        Dibujar();
+        Pintar();
+
+    }
+
+
+    void Update()
+    {
+        UpdateDraw();
+    }
+    private void UpdateDraw()
+    {
+        x2.Clear();
+        x2.vertices = vertices;
+        x2.RecalculateBounds();
+
+    }
+    IEnumerator CalulateShape()
+    {
+        vertices = new Vector3[(int)aux.terrainData.size.x*(int)aux.terrainData.size.x]; // el +2 es porque la clase vector3 pcuneta como 3 posiciones en el array [1]={0,1,2},{3,4,5};
+        for (x = 0; x < aux.terrainData.size.x; x++)
+        {
+            if (x % 10 == 0)
+            {
+                vertices[i++] = new Vector3(x, 1.7f, 0.6f);
+                con++;
+            }
+        }
+        tope = con;
+        for (z = 0; z < aux.terrainData.size.z; z++)
+        {
+            if (z % 10 == 0)
+            {
+                vertices[i++] = new Vector3(0.6f, 1.7f, z);
+                con++;
+            }
+        }
+        tope1 = con;
+        for (x = aux.terrainData.size.x; x > 0; x--)
+        {
+            if (x % 10 == 0)
+            {
+                vertices[i++] = new Vector3(x, 1.7f, aux.terrainData.size.z);
+                con++;
+            }
+        }
+        tope2 = con;
+        for (z = aux.terrainData.size.z; z > 0; z--)
+        {
+            if (z % 10 == 0)
+            {
+                vertices[i++] = new Vector3(aux.terrainData.size.x, 1.7f, z);
+                con++;
+            }
+        }
+        tope3 = con;
+        yield return new WaitForSeconds(.1f);
+    }
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+
+            return;
+        }
+        else
+        {
+            for (i = 0; i < vertices.Length; i++)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawCube(vertices[i], new Vector3(2, 5, 1.5f));
+            }
+        }
+    }
+    private void Dibujar()
+    {
+
+        // cammbia el tiling de la textura o dara fallos de parpadeo
+        var angles = transform.rotation.eulerAngles;
+        angles.y = 90;
+        newQuaternion = Quaternion.Euler(angles);
+
+        for (i = 0; i < con; i++)
+        {
+
+            if (i <= tope)
+            {
+                Instantiate(pared, vertices[i], Quaternion.identity);
+            }
+
+            if (i <= tope1&& i > tope)
+            {
+               
+               Instantiate(pared, vertices[i],newQuaternion);
+
+            }
+            if (i <= tope2&& i > tope1 && i > tope)
+            {
+                Instantiate(pared, vertices[i], Quaternion.identity);
+            }
+            if (i <= tope3&& i > tope2 && i > tope1 && i > tope)
+            {
+
+                Instantiate(pared, vertices[i], newQuaternion);
+            }
+        }
+
+    }
+    private void Pintar()
+    {
+        float[,,] MapaTexturas = new float[aux.terrainData.alphamapWidth, aux.terrainData.alphamapHeight, aux.terrainData.alphamapLayers];
+        for (int y = 0; y < aux.terrainData.alphamapHeight; y++)
+        {
+            for (int x = 0; x < aux.terrainData.alphamapWidth; x++)
+            {
+
+
+                float normX = x * 1.0f / (aux.terrainData.alphamapWidth - 1);
+                float normY = y * 1.0f / (aux.terrainData.alphamapHeight - 1);
+
+                
+                var angle = aux.terrainData.GetSteepness(normX, normY);
+
+                var frac = angle / 90.0;
+                MapaTexturas[x, y, 0] = (float)frac;
+                MapaTexturas[x, y, 1] = (float)(1 - frac);
+            }
+        }
+       aux.terrainData.SetAlphamaps(0, 0, MapaTexturas);
+    }
+}
+    
+
